244f51ed8ae0c160b1e7edf0b9634d3b3dc8d01e
llsengenheiro <llsengenheiro@gmail.com>
2020-04-01 19:32:50 -0300

    build with DATABSE URL to conecction 3

--- dist/database/index.js
+++ dist/database/index.js
@@ -5,7 +5,7 @@ var _Client = require('../app/models/Client'); var _Client2 = _interopRequireDef
 var _Service = require('../app/models/Service'); var _Service2 = _interopRequireDefault(_Service);
 var _Order = require('../app/models/Order'); var _Order2 = _interopRequireDefault(_Order);
 
-var _database = require('../config/database'); var _database2 = _interopRequireDefault(_database);
+// import databaseConfig from '../config/database';
 
 const models = [_User2.default, _Client2.default, _Service2.default, _Order2.default];
 
@@ -16,18 +16,28 @@ class Database {
 
   init() {
     if (process.env.DATABASE_URL) {
-      console.log('teste');
       // the application is executed on Heroku ... use the postgres database
       this.connection = new (0, _sequelize2.default)(process.env.DATABASE_URL, {
         dialect: 'postgres',
         protocol: 'postgres',
-        // port: match[4],
-        // host: match[3],
+        port: 5432,
+        host: 'ec2-52-6-143-153.compute-1.amazonaws.com',
         logging: true, // false
       });
     } else {
       // the application is executed on the local machine ... use mysql
-      this.connection = new (0, _sequelize2.default)(_database2.default);
+      this.connection = new (0, _sequelize2.default)({
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        define: {
+          timestamps: true,
+          underscored: true,
+          underscoredAll: true,
+        },
+      });
     }
 
     models
--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,7 @@ import Client from '../app/models/Client';
 import Service from '../app/models/Service';
 import Order from '../app/models/Order';
 
-import databaseConfig from '../config/database';
+// import databaseConfig from '../config/database';
 
 const models = [User, Client, Service, Order];
 
@@ -16,18 +16,28 @@ class Database {
 
   init() {
     if (process.env.DATABASE_URL) {
-      console.log('teste');
       // the application is executed on Heroku ... use the postgres database
       this.connection = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
         protocol: 'postgres',
-        // port: match[4],
-        // host: match[3],
+        port: 5432,
+        host: 'ec2-52-6-143-153.compute-1.amazonaws.com',
         logging: true, // false
       });
     } else {
       // the application is executed on the local machine ... use mysql
-      this.connection = new Sequelize(databaseConfig);
+      this.connection = new Sequelize({
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        define: {
+          timestamps: true,
+          underscored: true,
+          underscoredAll: true,
+        },
+      });
     }
 
     models
